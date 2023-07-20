cacd2b627bbb5831b9d4d149d2386f5b081eaf80
Vlad Sanyuk <vlad.sanyuk@gmail.com>
2023-07-20 10:51:02 +0300

    Ansible exercise 3: install Jenkins on EC2 AL

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -39,6 +39,22 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"] # Amazon
+}
+
 resource "aws_key_pair" "ec2_ssh_key" {
   key_name   = "ec2-key"
   public_key = file("~/.ssh/id_rsa.pub")
@@ -50,7 +66,7 @@ resource "aws_security_group" "ssh_80" {
   vpc_id      = data.aws_vpc.get_vpc.id
 
   ingress {
-    description = "Tomcat 8080"
+    description = "Jenkins 8080"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
@@ -78,9 +94,8 @@ resource "aws_security_group" "ssh_80" {
   }
 }
 
-
-resource "aws_instance" "server" {
-  count                       = 1
+resource "aws_instance" "ubuntu_server" {
+  count                       = 0
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
   associate_public_ip_address = true
@@ -89,6 +104,38 @@ resource "aws_instance" "server" {
   vpc_security_group_ids      = [aws_security_group.ssh_80.id]
 }
 
+resource "aws_instance" "amazon_linux_server" {
+  count                       = 1
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.ec2_ssh_key.key_name
+  subnet_id                   = local.subnets_ids[0]
+  vpc_security_group_ids      = [aws_security_group.ssh_80.id]
+}
+
+resource "null_resource" "run_ansible_amazon_linux" {
+  triggers = {
+    public_ips = join(",", aws_instance.amazon_linux_server[*].public_ip)
+  }
+
+  provisioner "local-exec" {
+    working_dir = "ansible"
+    command = length(aws_instance.amazon_linux_server) != 0 ? "ansible-playbook --inventory ${join(",", aws_instance.amazon_linux_server[*].public_ip)}, --user ec2-user install_run_jenkins.yaml" : "echo 'There is no Amazon Linux servers'"
+  }
+}
+
+resource "null_resource" "run_ansible_ubuntu" {
+  triggers = {
+    public_ips = join(",", aws_instance.ubuntu_server[*].public_ip)
+  }
+
+  provisioner "local-exec" {
+    working_dir = "ansible"
+    command = length(aws_instance.ubuntu_server) != 0 ? "ansible-playbook --inventory ${join(",", aws_instance.ubuntu_server[*].public_ip)}, --user ubuntu install_run_jenkins.yaml" : "echo 'There is no Ubuntu servers'"
+  }
+}
+
 output "ec2_ips" {
-  value = aws_instance.server[*].public_ip
+  value = concat(aws_instance.ubuntu_server[*].public_ip, aws_instance.amazon_linux_server[*].public_ip)
 }
